f8b533591713f2f30c0472f5f3976496ed013964
Xenya Nemkina <xenya.nemkina@yandex.ru>
2023-03-31 14:23:28 +0300

    featured: верстка аутентификации

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,10 @@ function Header() {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="лого Место" />
+      <div className="header__nav">
+        <p className="header__profile-email">Email@mail.ru</p>
+        <a className="header__link">Войти</a>
+      </div>
     </header>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,6 +7,19 @@ function Main({ cards, onEditProfile, onAddPlace, onEditAvatar, onCardClick, onC
 
   return (
     <main className="content">
+      <section className="auth">
+        <div className="auth__container">
+          <h2 className="auth__title">Регистрация</h2>
+          <form className="auth__form">
+            <input className="popup__field auth__field" type="email" name="email" placeholder="Email" minLength="2" autoComplete="off" required />
+            <span className="popup__error popup__error_active"></span>
+            <input className="popup__field auth__field" type="password" name="password" placeholder="Пароль" minLength="2" autoComplete="off" required />
+            <span className="popup__error popup__error_active"></span>
+            <button type="submit" className="popup__save popup__save_auth-button">Зарегистрироваться</button>
+            <p className="auth__text">Уже зарегистрированы? <a className="auth__text sign-up__link">Войти</a></p>
+          </form> 
+          </div>
+        </section>
       <section className="profile">
         <button className="profile__avatarbtn" type="button" onClick={onEditAvatar}></button>
         <img className="profile__avatar" src={currentUser.avatar} alt="фото профиля" />
